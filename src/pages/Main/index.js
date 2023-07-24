1933af26b2addd35a709ae12d39bff5b026cf8c4
parkjunyoung <joonyoung090@naver.com>
2023-07-24 23:13:49 +0900

    first commit

---
+++ src/pages/Main/index.js
@@ -0,0 +1,22 @@
+import "./main.scss";
+
+const Main = ()=>{
+    return (
+        <div className={"MainArea"}>
+            <div className={"mainTitleSection"}>
+                <p>
+                    안녕하세요. 사진과 기록을 좋아하는 주니어 웹 개발자 박준영입니다. <br />
+                    BackEnd, FrontEnd 가릴 것 없이 작업하는걸 좋아하며, 새로운 기술에 대하여 공부하는데 주저하지 않습니다. <br />
+                </p>
+                <h1>
+                    PARK JUN YOUNG<br />
+                    PORTFOLIO.
+                </h1>
+                <p>
+                    When I look back, I'm training myself so that the road I walked on can shine.
+                </p>
+            </div>
+        </div>
+    )
+}
+export default Main;
