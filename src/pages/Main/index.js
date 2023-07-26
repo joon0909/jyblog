8a15c7c36e90389d2328fdbc315e21e76c8b36ff
parkjunyoung <joonyoung090@naver.com>
2023-07-26 09:14:45 +0900

    issue#02 - Main Title aos 라이브러리 적용

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,22 +1,30 @@
 import "./main.scss";
+import {useEffect} from "react";
+import "../../module/test";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const Main = ()=>{
+    useEffect(() => {
+        AOS.init(); //안에 {duration: 2000} 넣어주면 전부 딜레이 2초 생김
+    })
     return (
         <div className={"MainArea"}>
             <div className={"mainTitleSection"}>
-                <p>
+                <p data-aos={"fade-up"} data-aos-duration={1000}>
                     안녕하세요. 사진과 기록을 좋아하는 주니어 웹 개발자 박준영입니다. <br />
                     BackEnd, FrontEnd 가릴 것 없이 작업하는걸 좋아하며, 새로운 기술에 대하여 공부하는데 주저하지 않습니다. <br />
                 </p>
-                <h1>
+                <h1 data-aos={"fade-down"} data-aos-duration={1000} data-aos-delay={300}>
                     PARK JUN YOUNG<br />
                     PORTFOLIO.
                 </h1>
-                <p>
+                <p data-aos={"fade-left"} data-aos-duration={1000} data-aos-delay={500}>
                     When I look back, I'm training myself so that the road I walked on can shine.
                 </p>
             </div>
         </div>
+
     )
 }
 export default Main;
